0d335674859c992d20f7e30b2f41c50782086e05
Benjamin Cook <bengcook55@gmail.com>
2022-12-01 22:35:36 -0500

    commit tree ddfd81eaa72be0ce3ad2cf229004278e2677496b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+// Add your cod
+
+const mainUrl = "http://localhost:3000/users"
+
+const body = document.querySelector('body');
+
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+    .catch(response => document.body.append('Unauthorized Access'))
+}
+function addNewContact(newID) {
+        document.getElementsByTagName('body')[0].innerHTML = newID;
+}
+submitData("Name", "name@name.com");
